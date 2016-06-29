173cca56ff02a4a5bc76b1f7c2f3ea575c710ed5
yanhsu <e31217@gmail.com>
2016-06-29 15:12:49 +0800

    router practice

--- shan/readme.md
+++ shan/readme.md
@@ -2,3 +2,4 @@
 // server.js 第二次練習 node server.js後 開啓瀏覽器輸入localhost port:8888 會看到hello World
 //function.js 第三次練習  //將函數say包在函數execute中並傳入一個值到函數execute中再導入say函數
 // index.js server.js 第四次練習//將server.js 包裝成一個模組後由index.js去驅動它
+//practice5_router.js practice5_server.js practice5_index.js 第五次練習 //將路由和伺服器整合起來
--- shan/server.js
+++ shan/server.js
@@ -1,15 +1,12 @@
 var http = require("http");
 
-function start() {
-  function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+function onRequest(request, response) {
+  console.log("Request received.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
 }
 
-exports.start = start;
+http.createServer(onRequest);
+
+console.log("Server has started.");
